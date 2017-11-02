75f9645a4a63f15dc03ab388256694e35f7dedfe
rewel cabiles <rewel.sicuan@gmail.com>
2017-11-02 21:42:07 +0800

    Added dynamic image scaling

--- bin/asset_loader.py
+++ bin/asset_loader.py
@@ -2,7 +2,22 @@ import pygame
 import json
 
 class Assets:
-	def load(name):
+	def __init__(self):
+		with open("bin/settings.json") as settings:
+			self.settings = json.load(settings)
+
 		with open('bin/assets.json') as assets:
-			assets = json.load(assets)
-		return pygame.image.load(assets[name]).convert_alpha()
+			self.assets = json.load(assets)
+
+	def load(self, name):
+		reso = self.settings['resolution']
+		print(reso)
+		image = pygame.image.load(self.assets[name]).convert_alpha()
+		if reso['x'] < 1920 and reso['y'] < 1080:
+			offset_width  = reso['x'] / 1920 * 100			
+			offset_height = reso['y'] / 1080 * 100
+		print (image.get_rect().x, image.get_rect().y)
+		image = pygame.transform.scale(image, (int(image.get_rect().width * (offset_width / 100)), int(image.get_rect().height * (offset_height/100))))
+
+		print(offset_height, offset_width)
+		return image
