07e080c9074cf57db6df9cd65a57fc732e1bb44a
rewel cabiles <rewel.sicuan@gmail.com>
2017-11-02 22:14:40 +0800

    resolution changes also apply to SCREEN coordinates now

--- bin/camera.py
+++ bin/camera.py
@@ -1,9 +1,19 @@
 import pygame
+import json
+
 class Camera:
 	def __init__(self):
 		self.x = 0
 		self.y = 0
 		self.follow = None
+		self.offset_width = 100
+		self.offset_height = 100
+		with open("bin/settings.json") as settings:
+			self.settings = json.load(settings)
+		reso = self.settings['resolution']
+		if reso['x'] < 1920 and reso['y'] < 1080:
+			self.offset_width  = reso['x'] / 1920 * 100			
+			self.offset_height = reso['y'] / 1080 * 100
 
 	def set_focus(self, sprite):
 		self.x = self.sprite.x
@@ -12,4 +22,4 @@ class Camera:
 	def display(self, screen, sprite_group):
 
 		for sprites in sprite_group:
-			screen.blit(sprites.image, (sprites.rect.x + self. x, sprites.rect.y + self.y))
+			screen.blit(sprites.image, ((sprites.rect.x * (self.offset_width / 100)) - self. x, (sprites.rect.y * (self.offset_height / 100)) - self.y))
