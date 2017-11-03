0de46bb5edb7aca1151c12b61b21b5fcffedcf41
rewel cabiles <rewel.sicuan@gmail.com>
2017-11-03 20:13:08 +0800

    Removed test ArcadeScreen Class

--- bin/world.py
+++ bin/world.py
@@ -28,20 +28,6 @@ class Screen:
 					print("CLICKED: ", sprites.name)
 					return sprites
 
-class ArcadeEntrance(Screen):
-	def __init__(self, world, camera):
-		Screen.__init__(self, world, camera)
-		self.create_rooms()
-
-	def create_rooms(self):
-		self.background = Backdrops('Arcade01')
-		self.background.set_layer(1)
-		self.chr = Clickable('Thomas01',800, 400, 'Thomas')
-		#self.chr.image= pygame.transform.scale(self.chr.image, (int(self.chr.rect.width*0.7), int(self.chr.rect.height*0.7)))
-		self.chr.set_layer(2)
-		self.sg_all.add(self.background)
-		self.sg_all.add(self.chr)
-		self.sg_clickables.add(self.chr)
 
 
 class World:
