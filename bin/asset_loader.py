f57eed5d6863f98de61a3ee7adb843b4e0b228d9
rewel cabiles <rewel.sicuan@gmail.com>
2017-11-04 22:04:36 +0800

    Separated image scaler, with image loader

--- bin/asset_loader.py
+++ bin/asset_loader.py
@@ -8,9 +8,19 @@ class Assets:
 		with open('bin/configs/assets.json') as assets:
 			self.assets = json.load(assets)
 
-	def load(self, name):
+	def load_image(self, name, scale=True):
 		
 		MasterImage = pygame.image.load(self.assets[name])
-		w, h = MasterImage.get_rect().width, MasterImage.get_rect().height
-		image = pygame.transform.scale(MasterImage, tools.world_to_screen(w, h))
+		image = MasterImage
+		if scale:
+			image = self.scale_image(MasterImage)
 		return image, MasterImage
+
+	def scale_image(self, master_image):
+		w, h = master_image.get_rect().width, master_image.get_rect().height
+		image = pygame.transform.scale(master_image, tools.world_to_screen(w, h))
+		return image
+
+	def load_font(self, name, size):
+		MasterFont = pygame.font.Font(self.assets[name], size)
+		return MasterFont
